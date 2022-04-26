bf34316ea53fbab741c2bd6436c64dd9731735ab
Henrik Alsér <henrik.alser@me.com>
2022-04-26 11:03:21 +0200

    Shared bus

---
+++ examples/src/bin/shared-bus.rs
@@ -0,0 +1,57 @@
+// $ cargo rb ferris
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use nrf_embassy as _; // global logger + panicking-behavior + memory layout
+
+use embassy::executor::Spawner;
+use embassy::time::{Delay, Duration, Timer};
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::{interrupt, spim, Peripherals};
+use embedded_graphics::{
+    image::{Image, ImageRaw, ImageRawLE},
+    pixelcolor::Rgb565,
+    prelude::*,
+};
+use st7735_embassy::{self, ST7735};
+use embedded_hal_async::spi::ExclusiveDevice;
+use nrf_embassy::shared_spi::SpiDeviceWithCs;
+
+#[embassy::main]
+async fn main(_spawner: Spawner, p: Peripherals) {
+    let mut config = spim::Config::default();
+    config.frequency = spim::Frequency::M32;
+    let irq = interrupt::take!(SPIM3);
+    // spim args: spi instance, irq, sck, mosi/SDA, config
+    //let spim = spim::Spim::new_txonly(p.SPI3, irq, p.P0_04, p.P0_28, config);
+    let spi_bus = Mutex::new(spim::Spim::new_txonly(p.SPI3, irq, p.P0_04, p.P0_28, config));
+    // cs_pin: chip select pin
+    let cs_pin = Output::new(p.P0_30, Level::Low, OutputDrive::Standard);
+    // let spi_dev = ExclusiveDevice::new(spim, cs_pin);
+    let spi_dev = SpiDeviceWithCs::new(&spi_bus, cs_pin);
+
+    // rst:  display reset pin, managed at driver level
+    let rst = Output::new(p.P0_31, Level::High, OutputDrive::Standard);
+    // dc: data/command selection pin, managed at driver level
+    let dc = Output::new(p.P0_29, Level::High, OutputDrive::Standard);
+
+    let mut display = ST7735::new(spi_dev, dc, rst, Default::default(), 160, 128);
+    display.init(&mut Delay).await.unwrap();
+    display.clear(Rgb565::BLACK).unwrap();
+
+    let image_raw: ImageRawLE<Rgb565> =
+        ImageRaw::new(include_bytes!("../../assets/ferris.raw"), 86);
+    let image: Image<_> = Image::new(&image_raw, Point::new(34, 24));
+    image.draw(&mut display).unwrap();
+    display.flush().await.unwrap();
+
+    // LED is set to max, but can be modulated with pwm to change backlight brightness
+    let mut backlight = Output::new(p.P0_03, Level::High, OutputDrive::Standard);
+    loop {
+        backlight.set_high();
+        Timer::after(Duration::from_millis(700)).await;
+        backlight.set_low();
+        Timer::after(Duration::from_millis(300)).await;
+    }
+}
