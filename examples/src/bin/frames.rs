179311164a743b85e9cab07cc3fc363b1ac25982
kalkyl <henrik.alser@me.com>
2022-12-24 14:20:10 +0100

    Cleanup

--- examples/src/bin/frames.rs
+++ examples/src/bin/frames.rs
@@ -22,25 +22,23 @@ use st7735_embassy::{self, Frame, ST7735IF};
 const BUF_SIZE: usize = 160 * 128 * 2;
 static FRAME_A: StaticCell<Frame<BUF_SIZE>> = StaticCell::new();
 static FRAME_B: StaticCell<Frame<BUF_SIZE>> = StaticCell::new();
-static NEXT_FRAME: StaticCell<Signal<ThreadModeRawMutex, &'static mut Frame<BUF_SIZE>>> =
-    StaticCell::new();
-static READY_FRAME: StaticCell<Signal<ThreadModeRawMutex, &'static mut Frame<BUF_SIZE>>> =
-    StaticCell::new();
+static NEXT_FRAME: Signal<ThreadModeRawMutex, &'static mut Frame<BUF_SIZE>> =
+    Signal::new();
+static READY_FRAME: Signal<ThreadModeRawMutex, &'static mut Frame<BUF_SIZE>> =
+    Signal::new();
 
 #[embassy_executor::task]
 async fn render(
     spi_dev: ExclusiveDevice<Spim<'static, SPI3>, Output<'static, P0_24>>,
     dc: Output<'static, P0_20>,
-    rst: Output<'static, P0_22>,
-    next_frame: &'static Signal<ThreadModeRawMutex, &'static mut Frame<BUF_SIZE>>,
-    ready_frame: &'static Signal<ThreadModeRawMutex, &'static mut Frame<BUF_SIZE>>,
+    rst: Output<'static, P0_22>
 ) {
     let mut display = ST7735IF::new(spi_dev, dc, rst, Default::default());
     display.init(&mut Delay).await.unwrap();
-    let mut frame = ready_frame.wait().await;
+    let mut frame = READY_FRAME.wait().await;
     loop {
-        next_frame.signal(frame);
-        frame = ready_frame.wait().await;
+        NEXT_FRAME.signal(frame);
+        frame = READY_FRAME.wait().await;
         display.flush_frame(&frame).await.unwrap();
     }
 }
@@ -60,25 +58,23 @@ async fn main(spawner: Spawner) {
     let dc = Output::new(p.P0_20, Level::High, OutputDrive::Standard);
     let rst = Output::new(p.P0_22, Level::High, OutputDrive::Standard);
 
-    let next_frame = NEXT_FRAME.init(Signal::new());
     let frame_a = FRAME_A.init(Default::default());
-    next_frame.signal(frame_a);
+    NEXT_FRAME.signal(frame_a);
 
-    let ready_frame = READY_FRAME.init(Signal::new());
     let frame_b = FRAME_B.init(Default::default());
-    ready_frame.signal(frame_b);
+    READY_FRAME.signal(frame_b);
 
-    defmt::unwrap!(spawner.spawn(render(spi_dev, dc, rst, next_frame, ready_frame)));
+    defmt::unwrap!(spawner.spawn(render(spi_dev, dc, rst)));
 
     let _backlight = Output::new(p.P0_13, Level::High, OutputDrive::Standard);
 
     let mut x = 0;
     let mut y = 0;
     loop {
-        let frame = next_frame.wait().await;
+        let frame = NEXT_FRAME.wait().await;
         // frame.clear(Rgb565::BLACK).unwrap();
         frame.set_pixel(x, y, Rgb565::GREEN);
-        ready_frame.signal(frame);
+        READY_FRAME.signal(frame);
         x = (x + 1) % 160;
         y = (y + 1) % 128;
         Timer::after(Duration::from_millis(10)).await;
